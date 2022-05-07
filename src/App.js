1f95feb7b7ccd90bf552b3e86bc170d102d2ae1f
ryan bautista <96922150+xryanb@users.noreply.github.com>
2022-05-06 17:13:10 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import ColorForm from './components/ColorForm';
+
+
+// const addColor =() =>{
+//   console.log('new')
+//   const tempColor=[...color];
+// }
+
+function App() {
+
+  return (
+
+    <div className="App">
+      <ColorForm/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ColorForm.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from  'react';
+
+const ColorForm = (props) => {
+    // These are state variables, they always go to the top of the component!
+    const [boxGen, setBoxGen] = useState([]);
+    const [textColor, setTextColor] = useState("")
+
+    const createBox = (e) => {
+        // add, prevent default behavior first thing.
+        e.preventDefault();
+        // write logic below
+        const newBoxGen = [...boxGen]
+        console.log(newBoxGen)
+        newBoxGen.push(textColor)
+        setBoxGen(newBoxGen)
+    };
+
+    return(
+        <div>
+        <form onSubmit={ createBox }>
+            <div>
+                <label>Color: </label>
+                <input value={textColor} onChange={ (e) => setTextColor(e.target.value) } />
+            </div>
+            <input type="submit" value="Add" />
+            <div>
+            </div>
+        </form>
+        
+        {
+            boxGen.map( (item, index) => {
+                return <div key = {index} style = {{backgroundColor: item, height: 100, width: 100, margin:15, display:"inline-flex"}} >
+                </div>
+            })
+        }
+        </div>
+    );
+};
+
+export default ColorForm;
